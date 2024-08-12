8dea5d2e8a51174045a26f6501c2b34f0ee43635
phuoctran92 <huuphuock10@gmail.com>
2024-08-12 11:18:30 +0700

    Flowbite svete TabItem issue

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,61 @@
 <script>
-  import { Heading, P, List, Li, Label, MultiSelect } from 'flowbite-svelte';
+  import { Tabs, TabItem, Button } from 'flowbite-svelte';
+
+  let tabItems = [
+    {
+      id: crypto.randomUUID(),
+      title: 'Tab 1',
+      content: 'Tab 1 Content'
+    },
+    {
+      id: crypto.randomUUID(),
+      title: 'Tab 2',
+      content: 'Tab 2 Content'
+    },
+    {
+      id: crypto.randomUUID(),
+      title: 'Tab 3',
+      content: 'Tab 3 Content'
+    },
+    {
+      id: crypto.randomUUID(),
+      title: 'Tab 4',
+      content: 'Tab 4 Content'
+    },
+    {
+      id: crypto.randomUUID(),
+      title: 'Tab 5',
+      content: 'Tab 5 Content'
+    },
+    {
+      id: crypto.randomUUID(),
+      title: 'Tab 6',
+      content: 'Tab 6 Content'
+    },
+  ];
+
+  function removeLastTab() {
+    if (tabItems.length > 0) {
+      tabItems = tabItems.slice(0, -1);
+    }
+  }
+
+  function handleRemoveTab(index) {
+    tabItems = tabItems.filter((_, i) => i !== index);
+  }
 </script>
 
-<div class='p-9'>
-<Heading tag="h1">Flowbite-Svelte Playground</Heading>
-<List tag="ol">
-  <Li class="text-2xl">Fork this Stackblitz project</Li>
-  <Li class="text-2xl">Update Flowbite-Svelte by running `pnpm i -D flowbite-svelte@latest`</Li>
-  <Li class="text-2xl">Run `pnpm update` to update dependencies.</Li>
-  <Li class="text-2xl">Add your code</Li>
-</List>
+<Tabs>
+  {#each tabItems as tab, idx}
+    <TabItem open={idx === 0} title={tab.title}>
+      <Button on:click={() => handleRemoveTab(idx)}>Delete</Button>
+      <p class="text-sm text-gray-500 dark:text-gray-400">
+        {tab.content}
+      </p>
+    </TabItem>
+  {/each}
+</Tabs>
 
-</div>
-
+<pre>
+{JSON.stringify(tabItems)}
+</pre>
